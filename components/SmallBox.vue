a3e53061a3dc2dbf860295d6e60d4d7272552564
nguyễn Thanh Tùng <thanhtung776@gmail.com>
2019-01-21 14:36:35 +0700

    cấu trúc cơ bản trang dashboard

---
+++ components/SmallBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="small-box bg-info">
+    <div class="inner">
+      <h3>{{card.title}}</h3>
+
+      <p>{{card.content}}</p>
+    </div>
+    <div class="icon">
+      <i :class="card.icon"></i>
+    </div>
+    <nuxt-link :to="card.link" class="small-box-footer">
+      {{card.footer}}
+      <i class="fa fa-arrow-circle-right"></i>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["card"]
+};
+</script>
